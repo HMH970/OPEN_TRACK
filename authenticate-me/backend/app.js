8624be57bc336db83ae48583a3bfcaa1fc4d3f78
MatthewHautala <november12erika@gmail.com>
2022-05-26 11:32:25 -0600

    oops

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-
+const { ValidationError } = require('sequelize');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
@@ -45,7 +45,21 @@ if (!isProduction) {
 
 app.use(routes); // Connect all the routes
 
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
+});
 
-
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = 'Validation error';
+  }
+  next(err);
+});
 
 module.exports = app;
