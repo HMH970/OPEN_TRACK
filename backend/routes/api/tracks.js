ee315c8894ca66042335b77dacf022ebfa0e320f
MatthewHautala <november12erika@gmail.com>
2022-05-31 13:33:49 -0600

    errors in api/index.js and tracks.js

--- backend/routes/api/tracks.js
+++ backend/routes/api/tracks.js
@@ -5,23 +5,20 @@ const bcrypt = require("bcryptjs")
 
 
 const db = require('../../db/models')
-const {User, Track, Review, Image, Booking} = db;
+;
 const { asyncHandler, csrfProptection, handleValidationErrors, check, validationResult} = require('../../utils')
 const { route } = require('.');
+// import { requireAuth } from '../../utils/auth';
 const app = require('../../app')
 // const {requireAuth} = require('../../utils')
 
 
-//route to get all the tracks
-router.get('/api/tracks', csrfProtection, asyncHandler(async(req, res) => {
-    if (req.session.auth === undefined) {
-        res.redirect('../')
-    }
-
-const userId = req.session.auth.userId;
-
+// route to get all the tracks
+router.get('/', csrfProtection, asyncHandler(async(req, res) => {
 const tracks = await db.Track.findAll()
 const images = await db.Image.findAll()
+console.log("tracks route hit")
+return res.json(tracks, images)
 }))
 
 module.exports = router;
