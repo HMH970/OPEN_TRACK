5bb54418fdb517b71ec5cf53c6ce2926a42e2423
MatthewHautala <november12erika@gmail.com>
2022-05-31 09:21:44 -0600

    trying this

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 
+
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
@@ -16,6 +17,18 @@ function App() {
   return (
     <>
       <Navigation isLoaded={isLoaded} />
+      <div className="track-type-slider-container">
+        <ul id="track-type-slider-ul" style={{"display": "flex", "listStyle": "none", "margin": "10px"}}>
+          <li className="track-type-slider-li" >Rental Karts</li>
+          <li className="track-type-slider-li" >Private Kart</li>
+          <li className="track-type-slider-li" >Auto</li>
+          <li className="track-type-slider-li" >MotoGp</li>
+          <li className="track-type-slider-li" >Drifting</li>
+        </ul>
+      </div>
+      <div className="track-list-container">
+
+      </div>
       {isLoaded && (
         <Switch>
           <Route path="/signup">
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import './Navigation.css';
+import * as sessionActions from "../../store/session"
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
+  const dispatch = useDispatch()
+  
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+  };
+
 
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <ProfileButton user={sessionUser} />
+      <>
+
+
+      <ProfileButton user={sessionUser}/>
+      <NavLink to="/" onClick={logout}>Log Out</NavLink>
+      </>
     );
   } else {
     sessionLinks = (
@@ -27,6 +40,7 @@ function Navigation({ isLoaded }){
       <li>
         <i class="fa-solid fa-flag-checkered"></i>
         <NavLink exact to="/">Home</NavLink>
+
         {isLoaded && sessionLinks}
       </li>
     </ul>
