3e1f2c424dc7b76cb37e50817f9d598e9c4e4216
MatthewHautala <november12erika@gmail.com>
2022-05-31 23:47:12 -0600

    boom db mf

--- backend/db/seeders/20220530170523-Tracks.js
+++ backend/db/seeders/20220530170523-Tracks.js
@@ -8,22 +8,22 @@ module.exports = {
      * Example:
      */
       return queryInterface.bulkInsert('Tracks', [
-        {userId: 11, name: "Grand Junction Motor SpeedWay", address: "3002 N I 70 Frontage Rd", city: "Grand Junction", state: "Colorado", country: "USA", phone: "(970) 256-0107", web: "http://www.grandjunctionmotorspeedway.com", price: "50"},
-        {userId: 11, name: "Utah Motorsports Campus", address: "512 Sheep Ln", city: "Erda", state: "Utah", country: "USA", phone: "(435) 277-8000", web: "http://www.utahmotorsportscampus.com", price: "60"},
-        {userId: 11, name: "SBR Motorsports Park", address: "21430 Spencer Rd", city: "Calhan", state: "Colorado", country: "USA", phone: "(719) 492-2635", web: "http://www.sbrmotorsportspark.com", price: "70"},
-        {userId: 11, name: "Action Karting", address: "3051 S Rooney Rd", city: "Morrison", state: "Colorado", country: "USA", phone: "(303) 781-4483", web: "http://www.actionkarting.net", price: "80"},
-        {userId: 11, name: "IMI Motorsports Complex", address: "5074 Summit Blvd", city: "Dacono", state: "Colorado", country: "USA", phone: "(303) 833-4949", web: "http://www.imimotorsports.com", price: "90"},
-        {userId: 11, name: "Pheonix Kart Racing Assoc (PKRA)", address: "22500 43rd Ave E", city: "Glendale", state: "Arizona", country: "USA", phone: "info@PKRA.com", web: "http://www.PKRA.com", price: "55"},
-        {userId: 11, name: "Musselman Honda Circuit", address: "11800 S Harrison Rd", city: "Tucson", state: "Arizona", country: "USA", phone: "(520) 245-5278", web: "http://www.mhcircuit.com", price: "65"},
-        {userId: 11, name: "Simraceway Kart Track", address: "29355 Arnold Dr", city: "Sonoma", state: "California", country: "USA", phone: "(800) 733-0345 ", web: "http://www.simracewaydrivingschool.com", price: "100"},
-        {userId: 11, name: "Sears Point Racing Experience", address: "29359 Arnold Dr", city: "Sonoma", state: "California", country: "USA", phone: "", web: "http://www.searspointracing.com", price: "50"},
-        {userId: 11, name: "Motorsports Park Hastings", address: "427 S Showboat Blvd", city: "Hastings", state: "Nebraska", country: "USA", phone: "(402) 303-6427", web: "http://www.racemph.com", price: "75"},
-        {userId: 11, name: "CalSpeed Karting Center", address: "9300 Cherry Ave", city: "Fontana", state: "California", country: "USA", phone: "(951) 506-9363", web: "http://www.calspeedkarting.com", price: "51"},
-        {userId: 11, name: "Apex Kart Racing", address: "18700 Lake Perris Dr", city: "Perris", state: "California", country: "USA", phone: "(951) 940-0701", web: "http://www.apexracingcenter.com", price: "52"},
-        {userId: 11, name: "GoPro Motorplex", address: "130 Motorplex Dr", city: "Mooresville", state: "North Carolina", country: "USA", phone: "(704) 696-2926", web: "http://www.gopromotorplex.com", price: "54"},
-        {userId: 11, name: "Andersen RacePark", address: "10101 US-41 N", city: "Palmetto", state: "Florida", country: "USA", phone: "(941) 723-3900", web: "http://www.andersenracepark.com", price: "53"},
-        {userId: 11, name: "Orlando Kart Center", address: "10724 Cosmonaut Blvd", city: "Orlando", state: "Florida", country: "USA", phone: "(407) 480-2477", web: "http://www.orlandokartcenter.com", price: "56"},
-        {userId: 11, name: "Bushnell Motorsports Park", address: "5821 Pit Rd", city: "Bushnell", state: "Florida", country: "USA", phone: "(352) 568-0778", web: "http://www.bushnellmotorsportpark.com", price: "59"}
+        {userId: 1, name: "Grand Junction Motor SpeedWay", address: "3002 N I 70 Frontage Rd", city: "Grand Junction", state: "Colorado", country: "USA", phone: "(970) 256-0107", web: "http://www.grandjunctionmotorspeedway.com", price: "50"},
+        {userId: 1, name: "Utah Motorsports Campus", address: "512 Sheep Ln", city: "Erda", state: "Utah", country: "USA", phone: "(435) 277-8000", web: "http://www.utahmotorsportscampus.com", price: "60"},
+        {userId: 1, name: "SBR Motorsports Park", address: "21430 Spencer Rd", city: "Calhan", state: "Colorado", country: "USA", phone: "(719) 492-2635", web: "http://www.sbrmotorsportspark.com", price: "70"},
+        {userId: 1, name: "Action Karting", address: "3051 S Rooney Rd", city: "Morrison", state: "Colorado", country: "USA", phone: "(303) 781-4483", web: "http://www.actionkarting.net", price: "80"},
+        {userId: 1, name: "IMI Motorsports Complex", address: "5074 Summit Blvd", city: "Dacono", state: "Colorado", country: "USA", phone: "(303) 833-4949", web: "http://www.imimotorsports.com", price: "90"},
+        {userId: 1, name: "Pheonix Kart Racing Assoc (PKRA)", address: "22500 43rd Ave E", city: "Glendale", state: "Arizona", country: "USA", phone: "info@PKRA.com", web: "http://www.PKRA.com", price: "55"},
+        {userId: 1, name: "Musselman Honda Circuit", address: "11800 S Harrison Rd", city: "Tucson", state: "Arizona", country: "USA", phone: "(520) 245-5278", web: "http://www.mhcircuit.com", price: "65"},
+        {userId: 1, name: "Simraceway Kart Track", address: "29355 Arnold Dr", city: "Sonoma", state: "California", country: "USA", phone: "(800) 733-0345 ", web: "http://www.simracewaydrivingschool.com", price: "100"},
+        {userId: 1, name: "Sears Point Racing Experience", address: "29359 Arnold Dr", city: "Sonoma", state: "California", country: "USA", phone: "", web: "http://www.searspointracing.com", price: "50"},
+        {userId: 1, name: "Motorsports Park Hastings", address: "427 S Showboat Blvd", city: "Hastings", state: "Nebraska", country: "USA", phone: "(402) 303-6427", web: "http://www.racemph.com", price: "75"},
+        {userId: 1, name: "CalSpeed Karting Center", address: "9300 Cherry Ave", city: "Fontana", state: "California", country: "USA", phone: "(951) 506-9363", web: "http://www.calspeedkarting.com", price: "51"},
+        {userId: 1, name: "Apex Kart Racing", address: "18700 Lake Perris Dr", city: "Perris", state: "California", country: "USA", phone: "(951) 940-0701", web: "http://www.apexracingcenter.com", price: "52"},
+        {userId: 1, name: "GoPro Motorplex", address: "130 Motorplex Dr", city: "Mooresville", state: "North Carolina", country: "USA", phone: "(704) 696-2926", web: "http://www.gopromotorplex.com", price: "54"},
+        {userId: 1, name: "Andersen RacePark", address: "10101 US-41 N", city: "Palmetto", state: "Florida", country: "USA", phone: "(941) 723-3900", web: "http://www.andersenracepark.com", price: "53"},
+        {userId: 1, name: "Orlando Kart Center", address: "10724 Cosmonaut Blvd", city: "Orlando", state: "Florida", country: "USA", phone: "(407) 480-2477", web: "http://www.orlandokartcenter.com", price: "56"},
+        {userId: 1, name: "Bushnell Motorsports Park", address: "5821 Pit Rd", city: "Bushnell", state: "Florida", country: "USA", phone: "(352) 568-0778", web: "http://www.bushnellmotorsportpark.com", price: "59"}
       ], {});
   },
 
--- backend/db/seeders/20220531161230-Images.js
+++ backend/db/seeders/20220531161230-Images.js
@@ -2,24 +2,24 @@
 
 module.exports = {
    up: (queryInterface, Sequelize) => {
-   
+
       return queryInterface.bulkInsert('Images', [
-        {trackId: 49, url: "https://www.qualityinngrandjunction.com/wp-content/uploads/speedway.jpg"},
-        {trackId: 50, url: "https://s3.us-east-1.amazonaws.com/k360uploads/images/20191104-umc-utah.jpg"},
-        {trackId: 51, url: "https://www.thecoloradokarter.com/wp-content/uploads/2018/10/The-Karter-Announcements-678x381.png"},
-        {trackId: 52, url: "https://www.uncovercolorado.com/wp-content/uploads/2016/09/Action-Karting-Morrison-1280x640.jpg"},
-        {trackId: 53, url: "https://ekartingnews.com/wp-content/uploads/2013/05/RMPKC-R3-S1S2.jpg"},
-        {trackId: 54, url: "https://ekartingnews.com/wp-content/uploads/2022/02/COTA-2022-PKRA-Sun-Hunter-Pickett.jpg"},
-        {trackId: 55, url: "https://img.grouponcdn.com/deal/5a7b0b8d810f440297919c9cf6fe441d/1d/v1/t440x300.jpg"},
-        {trackId: 56, url: "https://searspointracing.com/wp-content/uploads/2018/03/0J5A8738.jpg"},
-        {trackId: 57, url: "https://i.pinimg.com/originals/92/2f/ee/922fee8bbb58baafcb35b825f121b5dc.jpg"},
-        {trackId: 58, url: "https://visitnebraska.com/sites/default/files/styles/listing_slideshow/public/listing_images/profile/2514/mph_green_2.JPG?itok=WjKOhhXN"},
-        {trackId: 59, url: "https://www.gokartingtickets.com/wp-content/uploads/2020/06/CalspeedKartingCenter-foto2.jpg"},
-        {trackId: 60, url: "https://yellow.place/file/image/cover/0/0/344/hnyenwljsvwhqqxf.jpg"},
-        {trackId: 61, url: "https://www.gokartingtickets.com/wp-content/uploads/2020/06/gopro-motorplex-1-1.jpg"},
-        {trackId: 62, url: "https://www.andersenracepark.com/wp-content/uploads/2018/07/ARPClubRace-01-1024x515.png"},
-        {trackId: 63, url: "https://orlandokartcenter.com/wp-content/uploads/2020/12/OKC-2020.jpg"},
-        {trackId: 64, url: "https://bushnellmotorsportspark.com/wp-content/uploads/2020/04/This-Is.png"},
+        {trackId: 1, url: "https://www.qualityinngrandjunction.com/wp-content/uploads/speedway.jpg"},
+        {trackId: 2, url: "https://s3.us-east-1.amazonaws.com/k360uploads/images/20191104-umc-utah.jpg"},
+        {trackId: 3, url: "https://www.thecoloradokarter.com/wp-content/uploads/2018/10/The-Karter-Announcements-678x381.png"},
+        {trackId: 4, url: "https://www.uncovercolorado.com/wp-content/uploads/2016/09/Action-Karting-Morrison-1280x640.jpg"},
+        {trackId: 5, url: "https://ekartingnews.com/wp-content/uploads/2013/05/RMPKC-R3-S1S2.jpg"},
+        {trackId: 6, url: "https://ekartingnews.com/wp-content/uploads/2022/02/COTA-2022-PKRA-Sun-Hunter-Pickett.jpg"},
+        {trackId: 7, url: "https://img.grouponcdn.com/deal/5a7b0b8d810f440297919c9cf6fe441d/1d/v1/t440x300.jpg"},
+        {trackId: 8, url: "https://searspointracing.com/wp-content/uploads/2018/03/0J5A8738.jpg"},
+        {trackId: 9, url: "https://i.pinimg.com/originals/92/2f/ee/922fee8bbb58baafcb35b825f121b5dc.jpg"},
+        {trackId: 10, url: "https://visitnebraska.com/sites/default/files/styles/listing_slideshow/public/listing_images/profile/2514/mph_green_2.JPG?itok=WjKOhhXN"},
+        {trackId: 11, url: "https://www.gokartingtickets.com/wp-content/uploads/2020/06/CalspeedKartingCenter-foto2.jpg"},
+        {trackId: 12, url: "https://yellow.place/file/image/cover/0/0/344/hnyenwljsvwhqqxf.jpg"},
+        {trackId: 13, url: "https://www.gokartingtickets.com/wp-content/uploads/2020/06/gopro-motorplex-1-1.jpg"},
+        {trackId: 14, url: "https://www.andersenracepark.com/wp-content/uploads/2018/07/ARPClubRace-01-1024x515.png"},
+        {trackId: 15, url: "https://orlandokartcenter.com/wp-content/uploads/2020/12/OKC-2020.jpg"},
+        {trackId: 16, url: "https://bushnellmotorsportspark.com/wp-content/uploads/2020/04/This-Is.png"},
 
 
       ], {});
